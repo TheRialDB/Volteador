eaeaf51fca90f21c73db9e050490e5a0663861f0
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2022-07-11 16:13:27 -0300

    Agregar archivos de proyecto.

---
+++ Volteador/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Volteador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVoltear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnVoltear
+            // 
+            this.btnVoltear.Location = new System.Drawing.Point(162, 23);
+            this.btnVoltear.Name = "btnVoltear";
+            this.btnVoltear.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltear.TabIndex = 0;
+            this.btnVoltear.Text = "Voltear";
+            this.btnVoltear.UseVisualStyleBackColor = true;
+            this.btnVoltear.Click += new System.EventHandler(this.btnVoltear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Palabra a voltear";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 67);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 2;
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(12, 25);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(130, 20);
+            this.txtPalabra.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 136);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVoltear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtPalabra;
+    }
+}
+
---
+++ Volteador/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volteador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string palabra;
+        string resultado;
+        //Char[] modo2; // nuevo array
+
+        private void btnVoltear_Click(object sender, EventArgs e)
+        {
+            resultado = "";
+
+            palabra = txtPalabra.Text;
+
+            //modo2 = palabra.ToCharArray(); 
+
+            for (int i = palabra.Length-1; i >= 0; i--)
+            {
+                resultado = resultado + palabra.ElementAt(i).ToString();
+                //resultado = resultado + modo2[i];
+
+            }
+
+            if (resultado == "?")
+            {
+                resultado = "¿";
+            }
+            
+            lblResultado.Text = resultado;
+        }
+    }
+}
